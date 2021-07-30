97f737273569c93900b1884b5ee8cf72ec3d1336
Mikhail Bondarevsky <mibon@microsoft.com>
2021-07-30 13:55:23 +0300

    Add Track and Redirect controllers
    Add track and redirect controllers
    Add nuget packages for bot telemetry provider

---
+++ Source/CompanyCommunicator/Controllers/RedirectController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.Teams.Apps.CompanyCommunicator.Controllers
+{
+    [Route("redirect")]
+    [ApiController]
+    public class RedirectController : ControllerBase
+    {
+        private readonly ILogger<RedirectController> logger;
+        private readonly IBotTelemetryClient telemetryClient;
+
+        public RedirectController(ILoggerFactory loggerFactory, IBotTelemetryClient telemetryClient)
+        {
+            this.logger = loggerFactory?.CreateLogger<RedirectController>() ?? throw new ArgumentNullException(nameof(loggerFactory));
+            this.telemetryClient = telemetryClient ?? throw new ArgumentNullException(nameof(telemetryClient));
+        }
+
+        [HttpGet]
+        public IActionResult Get(string url, string id, string userId)
+        {
+            try
+            {
+                var props = new Dictionary<string, string> {
+                    { "notificationId", id },
+                    { "userId", userId },
+                    { "url", url },
+                };
+                this.telemetryClient.TrackEvent("TrackUrl", props);
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogError($"Exception={ex}");
+            }
+
+            return this.Redirect(url);
+        }
+    }
+}
---
+++ Source/CompanyCommunicator/Controllers/TrackController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.Teams.Apps.CompanyCommunicator.Controllers
+{
+    [Route("track")]
+    [ApiController]
+    public class TrackController : ControllerBase
+    {
+        private readonly ILogger<TrackController> logger;
+        private readonly FileContentResult pixelResponse;
+        private readonly IBotTelemetryClient telemetryClient;
+
+        public TrackController(FileContentResult pixelResponse, ILoggerFactory loggerFactory, IBotTelemetryClient telemetryClient)
+        {
+            this.pixelResponse = pixelResponse;
+            this.logger = loggerFactory?.CreateLogger<TrackController>() ?? throw new ArgumentNullException(nameof(loggerFactory));
+            this.telemetryClient = telemetryClient ?? throw new ArgumentNullException(nameof(telemetryClient));
+        }
+
+        [HttpGet]
+        public IActionResult Get(string url)
+        {
+            var delimiter = url.IndexOf('-');
+            try
+            {
+                if (delimiter > 0 && delimiter < url.Length)
+                {
+                    var properties = new Dictionary<string, string>()
+                    {
+                        { "notificationId", url.Substring(0, delimiter) },
+                        { "userId", url.Substring(delimiter + 1, url.Length - delimiter - 5) },
+                    };
+                    this.telemetryClient.TrackEvent("TrackView", properties);
+                }
+                else
+                {
+                    this.telemetryClient.TrackEvent("TrackUrl", new Dictionary<string, string> { { "url", url } });
+                }
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogError($"delimiter={delimiter}, length={url.Length}, url={url}, Exception={ex}");
+            }
+
+            return this.pixelResponse;
+        }
+    }
+}

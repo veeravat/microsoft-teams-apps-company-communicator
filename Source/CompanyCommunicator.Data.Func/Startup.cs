717b44eb99768f915a0030c77cf508f6f1da537a
Mikhail Bondarevsky <mibon@microsoft.com>
2021-08-05 13:33:09 +0300

    Update Startup.cs

--- Source/CompanyCommunicator.Data.Func/Startup.cs
+++ Source/CompanyCommunicator.Data.Func/Startup.cs
@@ -16,11 +16,13 @@ namespace Microsoft.Teams.Apps.CompanyCommunicator.Data.Func
     using Microsoft.Bot.Connector.Authentication;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Teams.Apps.CompanyCommunicator.Common.Clients;
     using Microsoft.Teams.Apps.CompanyCommunicator.Common.Repositories;
     using Microsoft.Teams.Apps.CompanyCommunicator.Common.Repositories.ExportData;
     using Microsoft.Teams.Apps.CompanyCommunicator.Common.Repositories.NotificationData;
     using Microsoft.Teams.Apps.CompanyCommunicator.Common.Repositories.SentNotificationData;
     using Microsoft.Teams.Apps.CompanyCommunicator.Common.Repositories.UserData;
+    using Microsoft.Teams.Apps.CompanyCommunicator.Common.Services.Blob;
     using Microsoft.Teams.Apps.CompanyCommunicator.Common.Services.CommonBot;
     using Microsoft.Teams.Apps.CompanyCommunicator.Common.Services.MessageQueues;
     using Microsoft.Teams.Apps.CompanyCommunicator.Common.Services.MessageQueues.DataQueue;
@@ -117,6 +119,9 @@ namespace Microsoft.Teams.Apps.CompanyCommunicator.Data.Func
 
             // Add service bus message queues.
             builder.Services.AddSingleton<IDataQueue, DataQueue>();
+
+            builder.Services.AddTransient<IStorageClientFactory, StorageClientFactory>();
+            builder.Services.AddTransient<IBlobStorageProvider, BlobStorageProvider>();
         }
     }
 }
